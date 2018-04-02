72ed9a0c04f40a0e5f2b8a5e89794943e081b0c0
Robert Allen <robby.allen24@gmail.com>
2018-04-01 22:48:41 -0400

    basic SQLAlchemy setup

--- server/config.py
+++ server/config.py
@@ -5,6 +5,7 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
